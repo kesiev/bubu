77006e182bef56490b45fdd85d24456a0caa2352
Francesco Cottone <kesiev@gmail.com>
2024-04-02 14:24:22 +0200

    :elephant: Sao chetini

---
+++ README.md
@@ -0,0 +1,33 @@
+# Bubu
+
+<div align="center"><p><img src="markdown/logo.png"></p></div><div align="center" style="font-weight:bold">A Italian-to-Bubu translator</div>
+
+---
+
+<div align="center"><a href="https://www.kesiev.com/bubu/">Translate to Bubu</a> <i>(Italian only)</i></div>
+
+---
+
+## The story
+
+A legend says that every puppet has its planet in which they look the same and live their lives together. Some dream of leaving their planet and exploring the Earth... but how can they do that without a spaceship? A few days after a real puppet joins your home, whether buying it or making it by yourself, one soul from its matching planet may enter its fluffy body from time to time, to see what we're doing. They can't move or talk - _obviously_ - but, if you _never stop dreaming_, you can _hear them_ and _know how they want to move their tiny bodies_!
+
+There are _turtles flying like UFOs_! There are _questioning giraffes_! There are _royal cats_! There even are _worms that can say just a single letter_!
+
+**Bubu** is one of them. It is an exuberant and very talkative tiny elephant, that has lived with us for years and loves cynically comment on our lives. It also loves saying _bad words_ from time to time without knowing the meaning because it just loves its sound.
+
+<div align="center" style="margin:60px 0">
+    <p><img src="markdown/bubu.png"></p>
+</div>
+
+It speaks Italian with a very high-pitched voice... but it has some defective pronunciation that is quite common _in some puppets_. Many words have the same sound so it's hard (and fun) to translate its words into Italian.
+
+## The project
+
+### Why?
+
+Never let the child inside you die. It's the most reliable judge of your life you will ever have.
+
+### Credits
+
+Thank you to **Bianca**, the mother and friend of many crazy puppets.
---
+++ js/translator.js
@@ -0,0 +1,442 @@
+let Translator=function(){
+
+    const
+    FILTERS={
+        bubu:[
+            // --- English
+            {
+                type:"replace",
+                from:/cy/gi,
+                to:"sai"
+            },{
+                type:"replace",
+                from:/punk/gi,
+                to:"panc"
+            },{
+                type:"replace",
+                from:/shake/gi,
+                to:"sciech"
+            },{
+                type:"replace",
+                from:/spear/gi,
+                to:"spir"
+            },{
+                type:"replace",
+                from:/white/gi,
+                to:"uait"
+            },{
+                type:"replace",
+                from:/something/gi,
+                to:"samfin"
+            },{
+                type:"replace",
+                from:/new/gi,
+                to:"niu"
+            },{
+                type:"replace",
+                from:/hea/gi,
+                to:"ha"
+            },{
+                type:"replace",
+                from:/design/gi,
+                to:"desain"
+            },{
+                type:"replace",
+                from:/writer/gi,
+                to:"vraiter"
+            },{
+                type:"replace",
+                from:/magazine/gi,
+                to:"megasin"
+            },{
+                type:"replace",
+                from:/play/gi,
+                to:"pei"
+            },{
+                type:"replace",
+                from:/down/gi,
+                to:"daun"
+            },
+
+            // --- CRI -> CHI -> (cretino -> chetino)
+            {
+                type:"replace",
+                from:/([cg])r([ei])/gi,
+                to:"$1h$2"
+            },
+
+            // --- RM -> MM (marmellata -> mammellata)
+            {
+                type:"replace",
+                from:/r([lvmt])/gi,
+                to:"$1$1"
+            },
+
+            // --- R ->  (radio -> adio)
+            {
+                type:"replace",
+                from:/(^|[ ?!,.;’'])r([^a ?!,.;’'])/gi,
+                to:"$1$2"
+            },
+            
+            // --- GRA -> BA (grande -> gande)
+            {
+                type:"replace",
+                from:/(^|[^r])r([aeiouàééìòù])/gi,
+                to:"$1$2"
+            },
+
+            // --- CCIA -> SSE (leccia -> lessia)
+            {
+                type:"replace",
+                from:/cc[ei]([aeiou])/gi,
+                to:"ss$1"
+            },
+
+            // --- CCE -> SSE (gocce -> gosse)
+            {
+                type:"replace",
+                from:/(^|[^e])cc([ei])/gi,
+                to:"$1ss$2"
+            },
+
+            // ACIA -> ASA (audacia -> audasa)
+            {
+                type:"replace",
+                from:/(^|[^u])ci([aeou])/gi,
+                to:"$1###$2"
+            },
+
+            // CE -> SE (audace -> audase)
+            {
+                type:"replace",
+                from:/(^|[^c])c([ei])/gi,
+                to:"$1s$2"
+            },
+
+            // --- CTO -> TTO (autoctono -> autottono)
+            {
+                type:"replace",
+                from:/c([t])/gi,
+                to:"$1$1"
+            },
+
+            // --- IZI -> ISSI (inizio -> INISSIO) 
+            {
+                type:"replace",
+                from:/([aeiou])z([^za])/gi,
+                to:"$1ss$2"
+            },
+
+            // --- AZZ -> ASS (mazza -> massa)
+            {
+                type:"replace",
+                from:/([^aeioulnp][e])zz([^u])/gi,
+                to:"$1sz$2"
+            },{
+                type:"replace",
+                from:/([^i])zz([^u])/gi,
+                to:"$1ss$2"
+            },{
+                type:"replace",
+                from:/(^|[^aeiours])z([^a])/gi,
+                to:"$1sz$2"
+            },
+
+            // --- NZA -> NSA -> (stanza -> stansa)
+            {
+                type:"replace",
+                from:/(^|[^aeiours])z([aeiou])/gi,
+                to:"$1###$2"
+            },{
+                type:"replace",
+                from:/([r])z([aeiou])/gi,
+                to:"$1###$2"
+            },
+            
+            // --- Fix "ZZ" -> ZSZ -> SZ
+            {
+                type:"replace",
+                from:/zsz/gi,
+                to:"sz"
+            },
+
+            // --- G/J -> Z
+            {
+                type:"replace",
+                from:/[jg]+e/gi,
+                to:"sze"
+            },
+
+            // --- N L -> LL (con le -> colle)
+            {
+                type:"replace",
+                from:/n l/gi,
+                to:"ll"
+            },
+
+            // --- R -> -> per il -> peil
+            {
+                type:"replace",
+                from:/r ([a-zA-Z])/gi,
+                to:"$1"
+            },
+
+            // --- GGIò -> ZZO (poggiò -> pozzò)
+            {
+                type:"replace",
+                from:/gg([aeio])([òaeiou])/gi,
+                to:"zz$2"
+            },{
+                type:"replace",
+                from:/([a])gg([i])/gi,
+                to:"$1zz$2"
+            },{
+                type:"replace",
+                from:/gm([aeiou])/gi,
+                to:"mm$1"
+            },
+
+            // --- GIA -> ZJ (giacca -> zjacca)
+            {
+                type:"replace",
+                from:/(^|[^aeiou])gi([aoue])/gi,
+                to:"$1zji$2"
+            },
+
+            // --- SN -> ZJN (snodo -> zjnodo)
+            {
+                type:"replace",
+                from:/sn/gi, // QUA
+                to:"zjn"
+            },{
+                type:"replace",
+                from:/([^aeiu])gi([aeiou])($|[ ?!,.;’'])/gi,
+                to:"$1zi$2$3"
+            },{
+                type:"replace",
+                from:/gi([àaeiou])/gi,
+                to:"z$1"
+            },
+            // IANGO -> IANZO (piango -> pianzo)
+            {
+                type:"replace",
+                from:/([aeiou]{2,})ng([eio])/gi,
+                to:"$1nz$2"
+            },
+
+            // --- RT -> TT (orto -> otto)
+            {
+                type:"replace",
+                from:/r([dpbftsncg])/gi,
+                to:"$1$1"
+            },{
+                type:"replace",
+                from:/(^|[^u])rr([aeiou])/gi,
+                to:"$1$2"
+            },
+            
+            // --- AGE -> AZE
+            {
+                type:"replace",
+                from:/[gj]i([^aeou])/gi,
+                to:"zi$1"
+            },
+
+            // OSI -> OZI
+            {
+                type:"replace",
+                from:/([aeo])s([aou])/gi,
+                to:"$1z$2"
+            },{
+                type:"replace",
+                from:/([aeou])s([^aàeèéiìoòuùzctpmf])/gi,
+                to:"$1$2$2"
+            },
+
+            // STI -> SI (capitalista -> capitalissa)
+            {
+                type:"replace",
+                from:/is([t])/gi,
+                to:"iss"
+            },
+
+            // --- PLA -> PA (platano -> patano)
+            {
+                type:"replace",
+                from:/(^|[^el])([pbuoieafr])l([aeiou])/gi,
+                to:"$1$2$3"
+            },
+
+            // --- CLA -> CA (classica -> cassica)
+            {
+                type:"replace",
+                from:/cl([uao])/gi,
+                to:"c$1"
+            },
+
+            // --- CLE -> CHE (pericle -> peiche)
+            {
+                type:"replace",
+                from:/cl([ei])/gi,
+                to:"ch$1"
+            },{
+                type:"replace",
+                from:/(^|[^a-z])gl([aeiou])([^a-z]|$)/gi,
+                to:"$1$2$3"
+            },
+
+            // --- NLA -> LLA (finlandese -> fillandese)
+            {
+                type:"replace",
+                from:/nl([aeiou])/gi,
+                to:"ll$1"
+            },
+
+            // --- GLA -> GHA (glicemia -> ghicemia)
+            {
+                type:"replace",
+                from:/(^|[^aeou])gl([ie])([aeiou])/gi,
+                to:"$1$2$3"
+            },{
+                type:"replace",
+                from:/(^|[^aeou])gl([ie])/gi,
+                to:"$1gh$2"
+            },{
+                type:"replace",
+                from:/(^|[^a])gl([uao])/gi,
+                to:"$1g$2"
+            },{
+                type:"replace",
+                from:/gli/gi,
+                to:"ii"
+            },
+
+            // --- ULLO -> UUO (sullo ->suuo)
+            {
+                type:"replace",
+                from:/(^|[^gc])([pbuo])ll([aeiou])/gi,
+                to:"$1$2$2$3"
+            },
+           
+            // --- LM -> MM
+            {
+                type:"replace",
+                from:/l([^aeiouz# ?!,.;’'])/gi,
+                to:"$1$1"
+            },{
+                type:"replace",
+                from:/l ([^aeiou0123456789])/gi,
+                to:"$1$1"
+            },
+
+            // --- L ->  (lira -> ira)
+            {
+                type:"replace",
+                from:/(^|[ ?!,.;’'])l([^a ?!,.;’'])/gi,
+                to:"$1$2"
+            },
+
+            // --- SV -> ZV
+            {
+                type:"replace",
+                from:/sv/gi,
+                to:"zv"
+            },
+
+            // --- CN -> NN (tecnologia -> tennologia)
+            {
+                type:"replace",
+                from:/cn([aeiou])/gi,
+                to:"nn$1"
+            },
+
+            // --- C'E -> SE
+            {
+                type:"replace",
+                from:/c['’]/gi,
+                to:"s"
+            },
+
+            // --- XA -> SSA (alexa -> alessa)
+            {
+                type:"replace",
+                from:/x([aeiou])/gi,
+                to:"ss$1"
+            },
+
+            // --- Fix "zzj"
+            {
+                type:"replace",
+                from:/([aeiou])z+j/gi,
+                to:"$1gg"
+            },
+
+            // --- Inglese
+            {
+                type:"replace",
+                from:/y/gi,
+                to:"i"
+            },{
+                type:"replace",
+                from:/wae/gi,
+                to:"ue"
+            },{
+                type:"replace",
+                from:/ck([aeiou])/gi,
+                to:"ch$1"
+            },{
+                type:"replace",
+                from:/w/gi,
+                to:"u"
+            },{
+                type:"replace",
+                from:/ork/gi,
+                to:"oc"
+            },
+            // --- Add forced S
+            {
+                type:"replace",
+                from:/####+/gi,
+                to:"ss"
+            },{
+                type:"replace",
+                from:/###/gi,
+                to:"s"
+            }
+        ]
+    };
+
+    function convert(sentence,type,debug,debugskip) {
+        FILTERS[type].forEach((filter,id)=>{
+            if ((debugskip== undefined) || (id != debugskip)) {
+                let
+                    prevSentence = sentence;
+                switch (filter.type) {
+                    case "replace":{
+                        for (let i=0;i<10;i++)
+                            sentence = sentence.replace(filter.from,filter.to)
+                        break;
+                    }
+                }
+                if ((debug || filter._debug) && (prevSentence != sentence)) {
+                    console.warn(prevSentence);
+                    console.log(filter);
+                    console.warn(">>>>",sentence);
+                }
+            }
+        });
+        return sentence;
+    }
+
+    this.FILTERS = FILTERS;
+
+    this.translate = function(sentence,type) {
+        return convert(sentence,type);
+    }
+
+    this.testTranslate = function(sentence,type,debug,debugskip) {
+        return convert(sentence,type,debug,debugskip);
+    }
+
+};
